7e9191eb698f03b272b52726e7b0dbaf26c8b3e5
aihara <petr_konyuk@mail.ru>
2020-11-02 20:15:58 +0200

    make it better

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Credentials: true');
 use App\App;
-header('Set-Cookie: key=value;SameSite=none;Secure');
+header('Set-Cookie: key=value;SameSite=lax;Secure');
 var_dump($_COOKIE);
 ini_set('display_errors', 'On');
 define('INC_ROOT', dirname(__DIR__));
